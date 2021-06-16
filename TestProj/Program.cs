d37672ae3923dc170fb8fdb195947f2b31b583fb
Robert Stueckler <Robert.Stueckler@liebherr.com>
2021-06-16 14:01:53 +0200

    Initial commit

---
+++ TestProj/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new();
+
+            Console.WriteLine("Please enter your desired Fueltype:");
+            if(string.IsNullOrEmpty(car.FuelType = Console.ReadLine()))
+            {
+                throw new Exception($"{nameof(car.FuelType)} cannot be empty");
+            }
+
+
+            Console.WriteLine("Please enter your desired amount of Horsepower");
+            try
+            {
+                car.Horsepower = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            Console.WriteLine("Please enter your desired amount of Seats");
+            try
+            {
+                car.NumberOfSeats = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            Console.WriteLine("Your new car will have:");
+            Console.WriteLine($"Fueltype: {car.FuelType}, Horsepower: {car.Horsepower}, Number of seats: {car.NumberOfSeats}");
+        }
+    }
+}
